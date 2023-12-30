0472b36426b0016f70efa0e9bb2449842358395e
tiago <82677798+mexerica@users.noreply.github.com>
2023-12-30 20:54:52 -0300

    dei uma estudada, agora eu acho que sei algo

--- Main.java
+++ Main.java
@@ -4,16 +4,21 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
+   private int[] memo = new int[65536]; 
+   private int PC = 0;
+   private int SP = 0;
+    
    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the file: ");
         String file = scan.nextLine(); 
-        scan.close();
+        scan.close();   
         try {
             FileReader reader = new FileReader(file);
             int data = reader.read();
+            String content = "";
             while (data != -1) {
-                System.out.println((char) data); 
+                content += (char) data;
                 data = reader.read();
             }
             reader.close();
@@ -22,5 +27,13 @@ public class Main {
         } catch (IOException e){
             e.printStackTrace();
         }
+   }
+
+   public int ReadByte( int address) {
+    return memo[address];
+   }
+
+   public void WriteByte(int address , int value) {
+    memo[address] = value;
    }
 }
