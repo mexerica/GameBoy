bf21db9e6f0711ab09b3a497eaad59dfe619f2e1
tiago <82677798+mexerica@users.noreply.github.com>
2024-09-20 23:04:46 -0300

    esqueci de arrumar antes

--- GameHeader.java
+++ GameHeader.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 
 public class GameHeader {
     public void Leitura(JFileChooser fileChooser) {
-        //File bootFile = new File("../[BIOS] Nintendo Game Boy Boot ROM (World).gb");
+        File bootFile = new File("[BIOS] Nintendo Game Boy Boot ROM (World).gb");
         File file = fileChooser.getSelectedFile();
         String data = "";
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(bootFile))) {
             for (String line = br.readLine(); line != null; line = br.readLine()) data += line;
         } catch (IOException e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
