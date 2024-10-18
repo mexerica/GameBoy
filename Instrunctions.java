6e7bc6e98d16dcd945dfe99f5f417708ad207223
tiago <82677798+mexerica@users.noreply.github.com>
2024-10-17 22:47:40 -0300

    me sentindo coringa 2 das ideias

--- Instrunctions.java
+++ Instrunctions.java
@@ -1,34 +1,69 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Instrunctions {
-    public int zeroFlag(int val) {
-        return val == 0 ? 1 : 0;
+   static int[] memo = new int[65536];
+   static int[] flags = {0, 0, 0, 0};// zero, subtraction, half carry and carry
+   static short[] registers = {257,0,0,0}; //AF, BC, DE, HL
+   //static byte registers = {0, 0, 0, 0, 0, 0, 0 ,0};
+   static ArrayList<Integer> stack = new ArrayList<Integer>();
+   static short PC = 0; // program counter 
+   static short SP = 0; // stack pointer
+   static boolean progress = true;
+   static boolean parar = false;
+
+    public int zeroFlag(int result) {
+        return result == 0 ? 1 : 0;
     }
 
-    public int subFlag(int val1, int val2) {
-        return val1 > val2 ? 1 : 0;
+    public int subFlag(int ogVal, int newVal) {
+        return ogVal > newVal ? 1 : 0;
     }
     
-    public int carryFlag(byte val1, byte val2) {
-        return (val1 + val2) > 127 || (val1 + val2) < -127 ? 1 : 0;
+    public int carryFlag(byte ogVal, byte newVal) {
+        return (ogVal + newVal) > 127 || (ogVal + newVal) < -127 ? 1 : 0;
     }
 
-    public int halfCarry(byte val) {
-        return 1;
+    public String halfCarry(short register) {
+        String binary = Integer.toBinaryString(0xFFFF & register);
+        while(binary.length() < 16) binary = "0" + binary;
+        String[] bits = new String[] {binary};
+        String firstHalf = String.join(",", Arrays.copyOfRange(bits, 0, 1));    
+        System.out.println(firstHalf);
+        return "";
     }
-    /*
+
+    public void Conversor16Para8Bits(short register) {
+        short valor16Bits = (short) 0xABCD; // Exemplo de valor de 16 bits
+
+        // Separando em bytes
+        byte byteAlto = (byte) (valor16Bits >> 8);
+        byte byteBaixo = (byte) valor16Bits;
+
+        System.out.println("Valor de 16 bits: " + valor16Bits);
+        System.out.println("Byte alto: " + byteAlto);
+        System.out.println("Byte baixo: " + byteBaixo);
+    }
+
     public void Instrunctions(String type, int target1, int target2){
         switch(type) {
+            case "nop" : PC++; break;
+            case "load" : break; 
+            case "halt" : parar = true; break;
             case "ADD" : registers[0] += registers[target1]; break;
             case "ADDX" : registers[target1] += registers[target2]; break;
-            case "ADDHL" : registers[0] += registers[target1] * 256 + virtualregisters[3]; break;
-            case "ADDC" : registers[0] += registers[target2] + (flags[3] ? 1 : 0); break;
+            case "ADDHL" : registers[3] += registers[target1]; break;
+            //case "ADDHL" : registers[0] += registers[target1] * 256 + virtualregisters[3]; break;
+            case "ADDC" : registers[0] += registers[target2] + flags[3]; break;
             case "SUB" : registers[0] -= registers[target1];break;
-            case "SBC" :registers[0] += registers[target2] + (flags[3] ? 1 : 0); break;
+            case "SBC" :registers[0] += registers[target2] + flags[3]; break;
             case "INC" : ++registers[target1]; break;
             case "DEC" : --registers[target1]; break;
-            case "CCF" : flags[3] = !flags[3]; break;
-            case "SCF" : flags[3] = true; break;
+            case "CCF" : flags[3] = (flags[3] == 1 ? 0 : 1); break;
+            case "SCF" : flags[3] = 1; break;
             case "RESET" : registers[target1]|= ~(1 << target2); break;
             case "SET" : registers[target1]|= 1 << target2; break;
+            //case "cp" : static compare = registers[target1] - registers[target2];
             case "CPL" : for (int temp = 1; temp <= registers[0]; temp = temp << 1) registers[0] ^= temp; break;
             case "AND" : registers[0] &= registers[target1]; break;
             case "OR" : registers[0] |= registers[target1]; break;
@@ -37,7 +72,9 @@ public class Instrunctions {
             case "BIT" : progress = ((registers[target1] & (1 <<target2)) != 0); break;
             case "SRA" : registers[target1] >>= 1; break;
             case "SLA" : registers[target1] <<= 1; break;
+            case "push" : stack.add(target1);
+            case "pop" : stack.remove(0);
             default : break;
         }
-   } */
+   } 
 }
--- Main.java
+++ Main.java
@@ -7,24 +7,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class Main {   
-   static int[] memo = new int[65536];
-   static int[] flags = {0, 0, 0, 0};// zero, subtraction, half carry and carry
-   static short[] registers = {0,0,0,0}; //AF, BC, DE, HL
-   //static byte registers = {0, 0, 0, 0, 0, 0, 0 ,0};
-   static short PC = 0; // program counter 
-   static short SP = 0; // stack pointer
-   
    public static void main(String[] args) {
-        Display Display = new Display();
-        Instrunctions Instrunctions = new Instrunctions();  
-        Display.Interface(registers, flags, PC, SP); 
+          Display Display = new Display();
+          Instrunctions Instrunctions = new Instrunctions(); 
+          short test = 400;
+          Display.Interface(Instrunctions.registers, Instrunctions.flags, Instrunctions.PC, Instrunctions.SP); 
+          Instrunctions.Conversor16Para8Bits(test);
    }
-
+   /*
    public int ReadByte(int address) {
         return memo[address];
    }
 
    public void WriteByte(int address , int value) {
         memo[address] = value;
-   }
+   }*/
 }
